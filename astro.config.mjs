4ed02bc4fa795fc2cbde7c53fea55ff90486308e
kotaro <kota1204996700@gmail.com>
2026-03-22 16:11:01 +0900

    Add GitHub Actions deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://kotaro9967.github.io",
+  base: "/Student-Council",
   integrations: [react(), tailwind()],
 });
